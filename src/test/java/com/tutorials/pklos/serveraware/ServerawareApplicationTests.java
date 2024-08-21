4f3eaf92ace524840a27c3736b7d0d7e2a69cfff
Pa3c <patryk.klos1997@gmail.com>
2024-08-21 23:07:55 +0200

    add mvc test for service info controller

--- src/test/java/com/tutorials/pklos/serveraware/ServerawareApplicationTests.java
+++ src/test/java/com/tutorials/pklos/serveraware/ServerawareApplicationTests.java
@@ -1,13 +1,27 @@
 package com.tutorials.pklos.serveraware;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
 class ServerawareApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/server/info"))
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("hostName").isNotEmpty())
+				.andExpect(jsonPath("hostAddress").isNotEmpty());
 	}
 
 }
